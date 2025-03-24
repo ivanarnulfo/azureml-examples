fcd4a3df56e4cc404fd4a8fe8e5b62740a245541
Ivan Montes <79025744+ivanarnulfo@users.noreply.github.com>
2025-03-23 21:05:26 -0400

    Update setup.sh

--- cli/setup.sh
+++ cli/setup.sh
@@ -25,9 +25,9 @@ az extension add -n ml -y
 
 ## For backward compatibility - running on old subscription
 # <set_variables>
-GROUP="azureml-examples"
-LOCATION="eastus"
-WORKSPACE="main"
+GROUP="Devops_Test_IM"
+LOCATION="eastus2"
+WORKSPACE="Azure_ML_workspace_IM"
 # </set_variables>
 
 # If RESOURCE_GROUP_NAME is empty, the az configure is pending.
